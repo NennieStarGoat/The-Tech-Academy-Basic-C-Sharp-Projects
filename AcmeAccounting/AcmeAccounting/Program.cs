29a3e5bf2cfab244c0d4317f1d22cbab5eeaf7e2
NennieStarGoat <jennieamsmith@gmail.com>
2026-06-25 13:46:32 -0700

    first small program

---
+++ AcmeAccounting/AcmeAccounting/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeAccounting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("\nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
